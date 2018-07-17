122db1dc792df4ed3f087ed53a8d42243b817000
Valeriya Klimenko <vklimenko@amcbridge.com>
2018-07-17 13:29:57 +0300

    remove comment

--- Source/Runtime/Impl/VariantContainer.hpp
+++ Source/Runtime/Impl/VariantContainer.hpp
@@ -119,11 +119,6 @@ namespace ursine
         template<typename T>
         void *VariantContainer<T>::GetPtr(void) const
         {
-            //TODO rewrite this cast
-            //this works incorrectly
-            //
-            //
-            
             return const_cast<void*>(
                 reinterpret_cast<const void*>( 
                     std::addressof( m_value )
